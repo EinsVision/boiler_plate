0c5b7fadd416eea10a7e7b78260a88c9dfc88985
Changdae <einsvision@gmail.com>
2021-04-07 11:51:26 +0900

    Postman test success ! built register page

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // back-end 시작점!! 
 // 1. node js download
-// 2. express js download  ( 5: 55)
+// 2. express js download
+// 3. body-parser (client로 부터 id, email, pwd 등의 정보를 읽어 들이는 것)
 
 const express = require('express') // express module을 가져옴
 const app = express() // 새로운 express app을 만든다.
 const port = 5000
 
-const {User} = require("./models/User");
+const { User } = require("./models/User");
 const bodyParser = require("body-parser");
 
 const config = require('./config/key');
@@ -35,11 +36,13 @@ app.post('/register', (req, res) => {
     // 회원 가입할 때 필요한 정보들을 client 에서 가져오면 
     // 그것들을 DB에 넣어준다.
     const user = new User(req.body);
+
+    // save <--- mongo db 에서 온 method 이다.
     user.save((err, userInfo)=>{
         if(err){
-            return res.json({success: false, err});
+            return res.json({ success: false, err });
         }
-        return res.status(200).json({success: true});    
+        return res.status(200).json({ success: true });    
     });
 })
 
