0c358ef1a91868b615c984b2680c4bf7e11d3d21
Changdae <einsvision@gmail.com>
2021-04-07 10:57:22 +0900

    Mongo DB model Schema 생성

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// Schema: 각 각의 정보를 변경할 수 있는 요소 
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         trim: true, // space 없애는 역활
-        unique: 1
+        unique: 1   // 똑같은 email은 쓰지 못하게 만든다.
     },
     password: {
         type: String,
@@ -21,6 +21,7 @@ const userSchema = mongoose.Schema({
     role: {
         type: Number,
         default: 0
+        // role 관리자가 될 수 도 있고, 일반 user가 될 수 도 있다.
     },
     image: {
         type: String
@@ -30,8 +31,9 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+        // token 유효기간을 설정한다.
     }
 })
 
-const User = mongoose.model('User', userSchema);
-module.exports = {User};
+const User = mongoose.model('User', userSchema); // Model을 schema로 감싼다.
+module.exports = { User };
