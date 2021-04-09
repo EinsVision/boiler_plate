df6ccc79999d7b74006851508aab1952aabcd2dc
Changdae <einsvision@gmail.com>
2021-04-09 09:49:50 +0900

    built auth router

--- index.js
+++ index.js
@@ -84,8 +84,20 @@ app.post('/api/users/login', (req, res) => {
   })
 })
 
-app.get('/api/users/auth', auth,(req, res) => {
+// role : 0 이면 일반 user
+// role : 0 이 아니면 관리자 user
 
+app.get('/api/users/auth', auth,(req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  })
 })
 
 app.listen(port, () => {
