820d8f89128c4b21870b6540d3ca3d4a70dbffce
Changdae <einsvision@gmail.com>
2021-04-09 14:36:10 +0900

    App.js routing using React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import LandingPage from './components/views/LandingPage/LandingPage';
+
 
 function App() {
+
   return (
-    <div className="app">
-      App js
-    </div>
+    <Router>
+      <div>
+        
+        <Switch>
+          <Route path="/about">
+            {/* <About /> */}
+          </Route>
+          <Route path="/users">
+            {/* <Users /> */}
+          </Route>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
