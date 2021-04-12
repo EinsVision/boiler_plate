3d053129e19c8db5363416f3f1d1de8fe53f52ee
Changdae <einsvision@gmail.com>
2021-04-12 13:16:12 +0900

    built login page

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_action/user_action';
 import './LoginPage.css';
 
-function LoginPage() {
+function LoginPage(props) {
+
+  const dispatch = useDispatch();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -16,6 +20,23 @@ function LoginPage() {
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    // console.log('email: ', email); // server에 보낼 값들을 state에서 가지고 있는 것이다.
+    // console.log('password: ', password);
+
+    let body = {
+      email: email,
+      password: password,
+    }
+
+    dispatch(loginUser(body))
+    .then(response => {
+      if(response.payload.loginSuccess) {
+        props.history.push('/');
+      } else {
+        alert('error in Login Page');
+      }
+    })
+    
   }
 
   return (
